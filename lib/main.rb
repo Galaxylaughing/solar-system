e3eafb1e3faaf2a154d6afdfc3da887365e86c21
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-08-19 12:30:05 -0700

    created main method in main.rb, instantiated two planets

--- lib/main.rb
+++ lib/main.rb
@@ -1,1 +1,17 @@
 require_relative "planet.rb"
+
+def main()
+  
+  earth = Planet.new("Earth", "blue-green", mass_kg: 5.972e24, distance_from_sun_km: 149600000, fun_fact: "the only planet known to support life")
+  
+  jupiter = Planet.new('Jupiter', 'stripey orange and white', mass_kg: 1.898e27, distance_from_sun_km: 778000000, fun_fact: 'twice as massive as all the other planets combined')
+  
+  earth_facts = "#{earth.name} is a #{earth.color} planet. Fun fact: #{earth.name} is #{earth.fun_fact}."
+  
+  jupiter_facts = "#{jupiter.name} is a #{jupiter.color} planet. Fun fact: #{jupiter.name} is #{jupiter.fun_fact}."
+  
+  puts earth_facts
+  puts jupiter_facts
+end
+
+print main()
--- lib/planet.rb
+++ lib/planet.rb
@@ -3,13 +3,12 @@ class Planet
   
   attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
   
-  def initialize(name, color, mass_kg:, distance_from_sun_km:, fun_fact = "no fun facts")
+  def initialize(name, color, mass_kg:, distance_from_sun_km:, fun_fact:)
     @name = name
     @color = color
     @mass_kg = mass_kg
     @distance_from_sun_km = distance_from_sun_km
     @fun_fact = fun_fact
   end
-  
   
 end
