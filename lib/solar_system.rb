8f0a96544288cd043ef6ef9b23e05ca8ff8a921e
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-08-19 13:50:01 -0700

    added tests for wave 2 step 1 and changed solar_system.rb to pass

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -1,4 +1,12 @@
 # each instance of SolarSystem will keep track of a collection of instances of Planet.
 
 class SolarSystem
+  
+  attr_reader :star_name, :planets
+  
+  def initialize(star_name:)
+    @star_name = star_name
+    @planets = []
+  end
+  
 end
--- specs/solar_system_spec.rb
+++ specs/solar_system_spec.rb
@@ -10,10 +10,27 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestSolarSystem < Minitest::Test
   def setup
-    @solar_system = SolarSystem.new
+    @solar_system = SolarSystem.new(star_name: "Arcturus")
   end
   
   def test_solar_system_instantiation
     refute_nil @solar_system
   end
+  
+  def test_saves_parameter_as_instance_variable
+    refute_nil @solar_system.star_name
+  end
+  
+  def test_variables_are_readable
+    assert_equal @solar_system.star_name, "Arcturus"
+  end
+  
+  def test_variables_are_not_writable
+    assert_raises(NoMethodError) { @solar_system.star_name = "Alpha" }
+  end
+  
+  def test_planets_variable_is_an_array
+    assert_instance_of Array, @solar_system.planets
+  end
+  
 end
